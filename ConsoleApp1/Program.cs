024405526fd6177eeb6f6e711cf09f7d7efae818
Patrick <Patrick@DESKTOP-ML5PASN>
2021-06-29 09:49:22 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // private void botaoDeposita_Click(object sender, EventsArgs e)
+            {
+                // Estrutura condicional.
+                int x = 10;
+                
+                Console.WriteLine("Bom dia");
+                if (x > 5)
+                {
+                    Console.WriteLine("Boa tarde");
+                }
+                
+                Console.WriteLine("Boa noite");
+
+            }
+            
+            
+            
+        }
+            
+    }
+}
